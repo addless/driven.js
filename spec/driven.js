94a80760c073b49461e0cb7e588299002a4b3b6a
janderson99 <james.anderson@bridgevine.com>
2017-01-16 07:36:19 -0800

    NGDC-3783 "driven.js" needs to report more granular error messages

--- spec/driven.js
+++ spec/driven.js
@@ -1,15 +1,16 @@
+
 describe('Driven.js', function () {
     'use strict';
 
     forEachTest('base/spec/sync.json', function () {
         var object;
 
-        runStep('create an object', function (key, args) {
-            object = new (Function.bind.apply(window[key], [null].concat(args)));
-        });
-
         runStep('manipulate the object', function (key, args) {
             object[key].apply(object, args);
+        });
+
+        runStep('create an object', function (key, args) {
+            object = new (Function.bind.apply(window[key], [null].concat(args)));
         });
 
         runStep("chain the object's method calls together", function (key, args) {
@@ -23,6 +24,10 @@ describe('Driven.js', function () {
         runStep("check the object's method return values", function (key, args, val) {
             expect(object[key].apply(object, args)).toEqual(val);
         });
+
+        runStep("check the specs's full name", function (name) {
+            expect(CustomReporter.specName).toEqual(name);
+        });
     });
 
     forEachTest('base/spec/async.json', function () {
@@ -33,12 +38,12 @@ describe('Driven.js', function () {
             endAsyncStep();
         });
 
-        runStep('bind callback arguments', function (args) {
-            callback = Function.bind.apply(callback, [null].concat(args));
-        });
-
         runAsyncStep('execute callback asynchronously', function (key, args) {
             id = window[key].apply(window, [callback].concat(args));
+        });
+
+        runStep('bind callback arguments', function (args) {
+            callback = Function.bind.apply(callback, [null].concat(args));
         });
 
         function callback() {
@@ -47,16 +52,34 @@ describe('Driven.js', function () {
             endAsyncStep();
         }
 
-        runStep('prevent collateral executions', function (key) {
-            window[key].call(window, id);
-        });
-
         runStep('check callback arguments', function (key, val) {
             expect(args[key]).toEqual(val);
+        });
+
+        runStep('prevent collateral executions', function (key) {
+            window[key].call(window, id);
         });
 
         runStep('check elapsed time', function (val) {
             expect(end - start).toBeGreaterThan(val);
         });
+
+        runStep("check the specs's full name", function (name) {
+            expect(CustomReporter.specName).toEqual(name);
+        });
     });
 });
+
+(function() {
+    'use strict';
+
+    jasmine.getEnv().addReporter(window.CustomReporter = {
+        specStarted: onSpecStarted
+    });
+
+    // This function records the full name of the current spec.
+    // Without it, we're unable to test that driven.js will report the appropriate error message.
+    function onSpecStarted(spec) {
+        this.specName = spec.fullName;
+    }
+}());
